ebf0d775cb6e34bd33695bb4c77302e7162a54ae
scnoder <thaipulleyrenesh@gmail.com>
2026-09-19 16:25:53 -0400

    Update ALLOWED_HOSTS to allow all hosts

--- TrafficAI/settings.py
+++ TrafficAI/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY") or os.getenv("SESSION_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
